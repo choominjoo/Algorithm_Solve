dad2eea3c0d28710952b14ede32964c57aa234dd
choominjoo <cutecmj04@gmail.com>
2025-08-15 17:53:02 +0900

    Add files via upload

---
+++ Baekjoon/그리디/2217.py
@@ -0,0 +1,13 @@
+N = int(input())
+ropes = [int(input()) for _ in range(N)]
+
+ropes.sort(reverse=True)
+
+max_weight = 0
+
+for i in range(N):
+    weight = ropes[i] * (i + 1)
+    if weight > max_weight:
+        max_weight = weight
+
+print(max_weight)
